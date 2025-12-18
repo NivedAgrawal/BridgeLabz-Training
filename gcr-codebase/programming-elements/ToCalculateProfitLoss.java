f2469889f1fc7373d8cee0003af8a2c957552a8b
NivedAgrawal <nivedagrawal0246@gmail.com>
2025-12-18 15:56:16 +0530

    [Nived Agrawal] Level 01 programns

---
+++ gcr-codebase/programming-elements/ToCalculateProfitLoss.java
@@ -0,0 +1,13 @@
+package ProgrammingElemrnt;
+
+public class ToCalculateProfitLoss {
+	public static void main(String args[]) {
+		int costPrice = 129;
+		int sellPrice = 191;
+		int profit = sellPrice - costPrice;
+		double profitPercentage = ((profit*100)/costPrice);
+		System.out.println("The Cost Price is INR " +costPrice+" and Selling Price is INR " + sellPrice);
+		System.out.println("Profit is INR " +profit+" and the Profit Percentage is "+profitPercentage);
+
+	}
+}
